e423a65ad6c667ac08c480065fd5892e54a6dff0
TrejGun <trejgun@gmail.com>
2022-02-06 19:09:22 +0800

    fix: update eslint

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,16 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "MongoDbBotStorage", {
-  enumerable: true,
-  get: function () {
-    return _MongoDBBotStorage.MongoDbBotStorage;
-  }
-});
 Object.defineProperty(exports, "MongoDBStorageClient", {
   enumerable: true,
   get: function () {
     return _MongoDBStorageClient.MongoDBStorageClient;
+  }
+});
+Object.defineProperty(exports, "MongoDbBotStorage", {
+  enumerable: true,
+  get: function () {
+    return _MongoDBBotStorage.MongoDbBotStorage;
   }
 });
 
